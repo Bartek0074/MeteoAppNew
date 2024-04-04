ef72df7dca76dbe5bf4c5912a1b9a2efdf9dd684
Bartek0074 <bartekdo123@gmail.com>
2024-04-04 19:56:37 +0200

    added NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,21 @@
 import styles from './NotFoundPage.module.scss';
 
+import { useNavigate } from 'react-router-dom';
+
+import { FaExclamationTriangle } from 'react-icons/fa';
+
 export default function NotFoundPage() {
-	return <div>NotFoundPage</div>;
+	const navigate = useNavigate();
+
+	const handleHomeClick = () => {
+		navigate('/');
+	};
+	return (
+		<div className={styles.notFoundPage}>
+			<FaExclamationTriangle className={styles.icon} />
+			<h1>404 - Page not found</h1>
+			<p>The page you are looking for does not exist.</p>
+			<button onClick={handleHomeClick}>Home Page</button>
+		</div>
+	);
 }
